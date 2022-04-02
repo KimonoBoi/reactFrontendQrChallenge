9cc963dfbe2abfc3d6263de84b86bcfee52cfc78
KimonoBoi <79030521+KimonoBoi@users.noreply.github.com>
2022-04-02 18:29:04 -0500

    upload files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import pic from './bokunoweb(1).png';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <div className="box">
+        <div className="whiteBox">
+          <div className="picBox">
+            <img src={pic} alt="boku no website"/>
+          </div>
+          <h3 className="label"> Creating new connections</h3>
+          <p className="des">This is really a link to my webside, I need to get more practice lol. ^U^</p>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default App;
